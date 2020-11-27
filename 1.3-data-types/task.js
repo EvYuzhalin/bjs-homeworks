59b6ecb6a12ceeb19e4731e55d94ef6718d21d7b
evgeniy <ev.yuhalin@gmail.com>
2020-11-27 22:23:14 +0500

    Add homework number 3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,55 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    checkIsNaN(percent, "Процентная ставка");
+    checkIsNaN(contribution, "Начальный взнос");
+    checkIsNaN(amount, "Общая стоимость");
+    checkIsNaN(date, "Срок ипотеки");
+  
+    let nowDate = new Date();
+  
+    let numberMonth = countMonth(nowDate, date);
+  
+    let bodyCredit = amount - contribution;
+    let p = percent / 100 / 12;
+  
+    let payPerMonth = bodyCredit * (p + p / (Math.pow(1 + p, numberMonth) - 1));
+  
+    let totalAmount = +(numberMonth * payPerMonth).toFixed(2);
+  
+    console.log(totalAmount);
+    //   console.log(`Переплата составит: ${(totalAmount - bodyCredit).toFixed(3)}`);
+  
+    // Вспомогательные функции:
+    function countMonth(startDate, endDate) {
+      let months;
+      months = (endDate.getFullYear() - startDate.getFullYear()) * 12;
+      months -= startDate.getMonth();
+      months += endDate.getMonth();
+  
+      return months <= 0 ? 0 : months;
+    }
+    function checkIsNaN(prametr, string) {
+      if (!prametr) {
+        return console.log(
+          `Параметр "${string}" содержит неправильное значение ${prametr}`
+        );
+      }
+    }
+  
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+
+    if (!isNaN(name) || name === undefined) {
+      greeting = `Привет, мир! Меня зовут Аноним`;
+    } else {
+      greeting = `Привет, мир! Меня зовут ${name}`;
+    }
+  
+    console.log(greeting);
+    
+    return greeting;
 }
