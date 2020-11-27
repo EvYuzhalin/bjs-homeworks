c1b524f2f0feea82ec5e6c8d7a249696d28254f9
evgeniy <ev.yuhalin@gmail.com>
2020-11-27 23:18:23 +0500

    add homework 5

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,41 @@
+"use strict";
+
 //String.prototype.isPalindrome - для задачи №1
 
+String.prototype;
+
+String.prototype.isPalindrome = function () {
+  const newString = this.toString().toLowerCase().replace(/ /g, "");
+
+  const newStringReverse = [...newString].reduce((prev, next) => next + prev);
+
+  let result = newString === newStringReverse ? true : false;
+
+  return result;
+};
+
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
+    let averageMark;
+    if (marks.length === 0) {
+      return 0;
+    } else {
+      return (averageMark = Math.round(
+        marks.reduce((x, y) => x + y) / marks.length
+      ));
+    }
     // return averageMark
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Math.round(Date.now());
+    birthday = Date.parse(birthday);
+  
+    let diff = now - birthday;
+  
+    const msInYear = 1000 * 60 * 60 * 24 * 365.25;
+    let age = diff / msInYear;
+  
+    let verdict = age >= 18 ? true : false;
+  
+    return verdict;
 }
